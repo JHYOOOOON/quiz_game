f6986d0e8bf700a9564e7993b03e821ee204e99c
JHYOOOOON <jhyooooooon@gmail.com>
2024-02-09 16:32:24 +0900

    feat: 색상 테마 정의 및 styled-components 설치

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ThemeProvider } from "styled-components";
+
+import * as Theme from "./theme";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
 	<React.StrictMode>
-		<App />
+		<ThemeProvider theme={Theme}>
+			<App />
+		</ThemeProvider>
 	</React.StrictMode>
 );
---
+++ src/theme/colors.ts
@@ -0,0 +1,6 @@
+export const colors = {
+	green: "rgb(0, 200, 150)",
+	white: "hsl(0,0%,100%)",
+	purple: "hsl(260,73%,70%)",
+	gray: "hsl(0,0%,26%)",
+};
