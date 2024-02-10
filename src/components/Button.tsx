a27f641cca79f9bb98e4935acef33a3308461bc9
JHYOOOOON <jhyooooooon@gmail.com>
2024-02-11 03:29:04 +0900

    feat: Button 사이즈 추가

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,26 +2,33 @@ import { ButtonHTMLAttributes, PropsWithChildren } from "react";
 import styled, { css } from "styled-components";
 
 type VariantType = "primary" | "secondary";
+type SizeType = "sm" | "md" | "lg";
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>, PropsWithChildren {
 	$fullWidth?: boolean;
 	$variant?: VariantType;
+	$size?: SizeType;
 }
 
-export function Button({ type, onClick, $fullWidth, $variant, children }: ButtonProps) {
+export function Button({ type, onClick, $fullWidth, $variant, $size, children, className }: ButtonProps) {
 	return (
-		<StyledButton type={type || "button"} onClick={onClick} $fullWidth={$fullWidth} $variant={$variant}>
+		<StyledButton
+			type={type || "button"}
+			onClick={onClick}
+			$fullWidth={$fullWidth}
+			$variant={$variant}
+			$size={$size}
+			className={className}
+		>
 			{children}
 		</StyledButton>
 	);
 }
 
-const StyledButton = styled.button<{ $fullWidth?: boolean; $variant?: VariantType }>`
-	padding: 15px 20px;
+const StyledButton = styled.button<{ $fullWidth?: boolean; $variant?: VariantType; $size?: SizeType }>`
 	border-radius: 5px;
 	border: none;
 	transition: 0.2s background;
-	font-size: 16px;
 	cursor: pointer;
 	${({ $fullWidth }) =>
 		$fullWidth &&
@@ -45,6 +52,23 @@ const StyledButton = styled.button<{ $fullWidth?: boolean; $variant?: VariantTyp
 			&:hover {
 				background-color: ${theme.colors.green700};
 			}
+		`;
+	}}
+	${({ $size }) => {
+		if ($size === "sm") {
+			return css`
+				padding: 5px 7px;
+				font-size: 14px;
+			`;
+		} else if ($size === "lg") {
+			return css`
+				padding: 15px 20px;
+				font-size: 18px;
+			`;
+		}
+		return css`
+			padding: 10px 15px;
+			font-size: 16px;
 		`;
 	}}
 `;
