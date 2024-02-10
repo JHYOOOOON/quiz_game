b507466c4c57c7bc4ffb70ef0848f9660b128770
JHYOOOOON <jhyooooooon@gmail.com>
2024-02-11 03:49:29 +0900

    fix: 잘못된 api 응답 타입 수정

--- src/types/responseTypes.ts
+++ src/types/responseTypes.ts
@@ -1,10 +1,10 @@
 import { CategoryList, QuestionList } from "./types";
 
 export type CategoryResponse = {
-	trivia_categories: CategoryList[];
+	trivia_categories: CategoryList;
 };
 
 export type QuestionResponse = {
 	response_code: string;
-	results: QuestionList[];
+	results: QuestionList;
 };
