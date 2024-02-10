60a727a4ecf0cffb1ac93d68ee29e230aaeb074a
JHYOOOOON <jhyooooooon@gmail.com>
2024-02-11 03:29:24 +0900

    fix: main 페이지 UI

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
-import { Select } from "../../components";
+import { Button, Header, Select } from "../../components";
 import { Difficulty, Type, WithAny } from "../../types";
+import styled from "styled-components";
 
 const DIFFICULTY = {
 	any: "전체",
@@ -21,36 +22,84 @@ export function Main() {
 	const [type, setType] = useState<WithAny<Type>>("any");
 
 	return (
-		<div test-id="main">
-			<p>문제 개수</p>
-			<Select target={amount} onTargetChange={(target: string) => setAmount(target)}>
-				<Select.Trigger>{amount}</Select.Trigger>
-				<Select.Content>
-					<Select.Option label={"5"}>5</Select.Option>
-					<Select.Option label={"10"}>10</Select.Option>
-					<Select.Option label={"15"}>15</Select.Option>
-					<Select.Option label={"20"}>20</Select.Option>
-				</Select.Content>
-			</Select>
-			<p>난이도</p>
-			<Select target={difficulty} onTargetChange={(target: string) => setDifficulty(target as WithAny<Difficulty>)}>
-				<Select.Trigger>{DIFFICULTY[difficulty]}</Select.Trigger>
-				<Select.Content>
-					<Select.Option label={"any"}>{DIFFICULTY["any"]}</Select.Option>
-					<Select.Option label={"easy"}>{DIFFICULTY["easy"]}</Select.Option>
-					<Select.Option label={"medium"}>{DIFFICULTY["medium"]}</Select.Option>
-					<Select.Option label={"hard"}>{DIFFICULTY["hard"]}</Select.Option>
-				</Select.Content>
-			</Select>
-			<p>문제 유형</p>
-			<Select target={type} onTargetChange={(target: string) => setType(target as WithAny<Type>)}>
-				<Select.Trigger>{TYPE[type]}</Select.Trigger>
-				<Select.Content>
-					<Select.Option label={"any"}>{TYPE["any"]}</Select.Option>
-					<Select.Option label={"multiple"}>{TYPE["multiple"]}</Select.Option>
-					<Select.Option label={"boolean"}>{TYPE["boolean"]}</Select.Option>
-				</Select.Content>
-			</Select>
-		</div>
+		<Wrapper test-id="main">
+			<Header />
+			<Title>퀴즈</Title>
+			<Section>
+				<SelectWrapper>
+					<div>
+						<Label>문제 개수</Label>
+						<Select target={amount} onTargetChange={(target: string) => setAmount(target)}>
+							<Select.Trigger>{amount}</Select.Trigger>
+							<Select.Content>
+								<Select.Option label={"5"}>5</Select.Option>
+								<Select.Option label={"10"}>10</Select.Option>
+								<Select.Option label={"15"}>15</Select.Option>
+								<Select.Option label={"20"}>20</Select.Option>
+							</Select.Content>
+						</Select>
+					</div>
+					<div>
+						<Label>난이도</Label>
+						<Select
+							target={difficulty}
+							onTargetChange={(target: string) => setDifficulty(target as WithAny<Difficulty>)}
+						>
+							<Select.Trigger>{DIFFICULTY[difficulty]}</Select.Trigger>
+							<Select.Content>
+								<Select.Option label={"any"}>{DIFFICULTY["any"]}</Select.Option>
+								<Select.Option label={"easy"}>{DIFFICULTY["easy"]}</Select.Option>
+								<Select.Option label={"medium"}>{DIFFICULTY["medium"]}</Select.Option>
+								<Select.Option label={"hard"}>{DIFFICULTY["hard"]}</Select.Option>
+							</Select.Content>
+						</Select>
+					</div>
+					<div>
+						<Label>문제 유형</Label>
+						<Select target={type} onTargetChange={(target: string) => setType(target as WithAny<Type>)}>
+							<Select.Trigger>{TYPE[type]}</Select.Trigger>
+							<Select.Content>
+								<Select.Option label={"any"}>{TYPE["any"]}</Select.Option>
+								<Select.Option label={"multiple"}>{TYPE["multiple"]}</Select.Option>
+								<Select.Option label={"boolean"}>{TYPE["boolean"]}</Select.Option>
+							</Select.Content>
+						</Select>
+					</div>
+				</SelectWrapper>
+				<Button $size="lg" $fullWidth>
+					퀴즈 풀기
+				</Button>
+			</Section>
+		</Wrapper>
 	);
 }
+
+const Wrapper = styled.div`
+	height: 100%;
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+`;
+
+const Title = styled.h1`
+	text-align: center;
+	font-size: 50px;
+	font-weight: bold;
+`;
+
+const Section = styled.section`
+	margin: 20px 15px;
+	display: flex;
+	flex-direction: column;
+	gap: 20px;
+`;
+
+const Label = styled.p`
+	margin-bottom: 5px;
+`;
+
+const SelectWrapper = styled.div`
+	display: flex;
+	flex-direction: column;
+	gap: 10px;
+`;
