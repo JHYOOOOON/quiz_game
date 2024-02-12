b0da4c934c15a9cb7ea974faf7d8842e80830ff2
JHYOOOOON <jhyooooooon@gmail.com>
2024-02-12 15:30:04 +0900

    feat: test template react-query 설정

--- src/pages/tests/Template.tsx
+++ src/pages/tests/Template.tsx
@@ -1,18 +1,29 @@
 import React from "react";
 import { ThemeProvider } from "styled-components";
 import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import GlobalStyles from "../../globalStyles";
 import * as Theme from "../../theme";
 
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			refetchOnWindowFocus: false,
+		},
+	},
+});
+
 const Template = ({ children }: React.PropsWithChildren) => {
 	return (
-		<BrowserRouter>
-			<ThemeProvider theme={Theme}>
-				<GlobalStyles />
-				{children}
-			</ThemeProvider>
-		</BrowserRouter>
+		<QueryClientProvider client={queryClient}>
+			<BrowserRouter>
+				<ThemeProvider theme={Theme}>
+					<GlobalStyles />
+					{children}
+				</ThemeProvider>
+			</BrowserRouter>
+		</QueryClientProvider>
 	);
 };
 
