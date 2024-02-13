193b6e367222383d9abe2e4377df2fb5cf14d24c
JHYOOOOON <jhyooooooon@gmail.com>
2024-02-13 19:56:30 +0900

    feat: 정해진 경로 외에는 메인으로 리다이렉션

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import { ROUTES } from "./constants";
 import { Main, Quiz, Result } from "./pages";
 
@@ -9,6 +9,7 @@ export function Router() {
 			<Route path={ROUTES.MAIN} element={<Main />} />
 			<Route path={ROUTES.QUIZ} element={<Quiz />} />
 			<Route path={ROUTES.RESULT} element={<Result />} />
+			<Route path="*" element={<Navigate to={ROUTES.MAIN} replace />} />
 		</Routes>
 	);
 }
