e65a7be47a01784558aeb99c681a10a35a20c1c4
JHYOOOOON <jhyooooooon@gmail.com>
2024-02-13 16:49:14 +0900

    test: progressbar 테스트코드 작성

---
+++ src/components/tests/Progressbar.test.tsx
@@ -0,0 +1,20 @@
+import { render, screen } from "@testing-library/react";
+import Template from "../../pages/tests/Template";
+import { ProgressBar } from "../ProgressBar";
+import { getPercentage } from "../../utils";
+
+test("Progressbar가 올바르게 렌더링되는지 확인합니다.", () => {
+	const total = 5;
+	const step = 0;
+
+	render(
+		<Template>
+			<ProgressBar total={total} step={step} />
+		</Template>
+	);
+
+	const progressBar = screen.getByTestId("progress");
+
+	expect(progressBar).toHaveStyle(`width: ${getPercentage(total, step)}%`);
+	expect(progressBar).toHaveStyle(`width: 20%`);
+});
